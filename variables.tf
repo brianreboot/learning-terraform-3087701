47e7cf2fff8173af61229f24a3d74b61577e2d13
brianreboot <59297207+brianreboot@users.noreply.github.com>
2023-09-14 11:34:29 +0900

    instance types

--- variables.tf
+++ variables.tf
@@ -1,9 +1,9 @@
-variable "instance_t3_nano" {
+variable "t3_nano" {
   description = "Type of EC2 instance to provision"
   default     = "t3.nano"
 }
 
-variable "instance_t3_micro" {
+variable "t3_micro" {
   description = "Type of EC2 instance to provision"
   default     = "t3.micro"
 }
