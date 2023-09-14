945f1edd45fe44264322e938c226989add170246
brianreboot <59297207+brianreboot@users.noreply.github.com>
2023-09-14 11:28:19 +0900

    add machines

--- variables.tf
+++ variables.tf
@@ -1,4 +1,9 @@
-variable "instance_type" {
+variable "instance_t3.nano" {
   description = "Type of EC2 instance to provision"
   default     = "t3.nano"
+}
+
+variable "instance_t3.micro" {
+  description = "Type of EC2 instance to provision"
+  default     = "t3.micro"
 }
