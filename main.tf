afda7d38470d6b5b30f4b1f9cd3b4e575719a736
brianreboot <59297207+brianreboot@users.noreply.github.com>
2023-09-20 14:46:49 +0900

    import vpc

--- main.tf
+++ main.tf
@@ -14,10 +14,7 @@ data "aws_ami" "app_ami" {
   owners = ["348016465244"]
 }
 
-import {
-  to = aws_vpc.terraform_vpc
-  id = "vpc-017882a3fcce1c41c"
-}
+terraform import aws-vpc.terraform_vpc vpc-017882a3fcce1c41c
 
 resource "aws_instance" "web" {
   ami           = data.aws_ami.app_ami.id
