2bd44f95dfe124b2f03e8038a6257f9ed41f4e7b
Your Name <you@example.com>
2023-09-21 16:15:36 +0900

    new vpc

--- main.tf
+++ main.tf
@@ -1,3 +1,18 @@
+resource "aws_vpc" "terraform-vpc" {
+    assign_generated_ipv6_cidr_block     = false
+    cidr_block                           = "10.0.0.0/16"
+    enable_dns_hostnames                 = true
+    enable_dns_support                   = true
+    enable_network_address_usage_metrics = false
+    instance_tenancy                     = "default"
+    tags                                 = {
+        "Name" = "terraform-vpc"
+    }
+    tags_all                             = {
+        "Name" = "terraform-vpc"
+    }
+}
+
 data "aws_ami" "app_ami" {
   most_recent = true
 
